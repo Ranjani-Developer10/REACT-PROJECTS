2049cb7c035408e05f7a4364517623097be421d9
SWATHI N <swathi.pkn@gmail.com>
2026-06-16 14:29:29 +0530

    addtask

--- src/pages/EmployeReg.jsx
+++ src/pages/EmployeReg.jsx
@@ -27,27 +27,9 @@ function EmployeReg() {
       <h1>Employee Registration Form</h1>
 
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Employee Name"
-          value={employeeName}
-          onChange={(e) => setEmployeeName(e.target.value)}
-        />
-
-        <input
-          type="text"
-          placeholder="Department"
-          value={department}
-          onChange={(e) => setDepartment(e.target.value)}
-        />
-
-        <input
-          type="number"
-          placeholder="Salary"
-          value={salary}
-          onChange={(e) => setSalary(e.target.value)}
-        />
-
+        <input type="text" placeholder="Employee Name" value={employeeName} onChange={(e) => setEmployeeName(e.target.value)} />
+        <input type="text" placeholder="Department" value={department} onChange={(e) => setDepartment(e.target.value)}/>
+        <input type="number" placeholder="Salary" value={salary} onChange={(e) => setSalary(e.target.value)}/>
         <button type="submit">Add Employee</button>
       </form>
 
